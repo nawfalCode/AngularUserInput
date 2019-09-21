4deb815199a7e4ec82c1aa409ed594d9275e7001
Nawfal Ali <nawfal.ali@monash.edu>
2019-09-21 22:15:07 +1000

    dd get data three cases

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'angularinput';
+  case1: string = "";
+  case2: string = "";
+  case3: string = "";
+  inputChange(event: any): void {
+    this.case1 += event.target.value + "|";
+  }
+  inputKeyUp(event: any): void {
+    this.case2 += event.target.value + "|";
+  }
+  templateReferenceChange(value: String): void {
+    this.case3 += value + "|";
+  }
 }
